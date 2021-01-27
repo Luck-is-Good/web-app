e880eb40671c8f1fabb6d8febe088fe1b5da3838
Simon Lim <srlim1121@gmail.com>
2021-01-27 23:28:24 +0900

    Add:reducer set

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,15 +1,15 @@
 /* eslint-disable */
 import Reducer_setting from './reducer_setting';//state 초기값을 가져온다.
 import * as types from '../actions/actionTypes'; //액션 코드로 가져온다.
- 
+
 const root_reducer = {
     data: Reducer_setting
-} 
+}
 
 function reducer (state = root_reducer, action){ 
     //state는 reducer가 갖고 있는 state고 만약 없을시 초기값을 가져온다.
     //action은 dispatch함수로 connect를 통해서 들어 오게 된다.
-    const {setting} = state;
+    const {data} = state;
 
     switch (action.type){//리듀서를 통해서 들어온 action 
         case types._CREATE_USER: //acionTyeps에서 구분
@@ -31,6 +31,10 @@ function reducer (state = root_reducer, action){
         case types._UPDATE_USER_LIST:
             return {
 
+            }
+        case types._INIT_STORE:
+            return {data:
+                {...data}.concat(action.data)
             }
         default: return state; //action을 이용하지 않을때 기본 this.props.setting을 사용할 때 사용
     }
