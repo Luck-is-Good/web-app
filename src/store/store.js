5a73f14e855870fdba6435d58dfaceba824d3d57
Simon Lim <srlim1121@gmail.com>
2021-01-27 23:37:05 +0900

    Merge: merge collision fixed2

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,7 @@
 /* eslint-disable */
 import { createStore } from "redux"
 import reducers from '../reducers/reducer'
-<<<<<<< HEAD
-var store = createStore(reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-=======
+
 var store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
->>>>>>> e502979bc1f05e16b75650e60af9cc1b78c3f6d5
 export default store;
 
